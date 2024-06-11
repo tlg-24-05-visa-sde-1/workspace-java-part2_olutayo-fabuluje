de843bb3124eb62bc2290e8a4eb738373603cfb3
Olutayo Fabuluje <olutayof@tlgcohort.com>
2024-06-11 15:27:38 -0500

    Order and TaxCalculator family for getTax()

--- Lab05.3/src/com/javatunes/billing/EuropeTax.java
+++ Lab05.3/src/com/javatunes/billing/EuropeTax.java
@@ -8,13 +8,27 @@
  */
 package com.javatunes.billing;
 
-/**
+/*
  * European orders are taxed as follows:
  * VAT is 17% of taxable amount.
  * Luxury tax is an additional 25% on amount over $100.
- * 
- * TODO: implement this algorithm.
  */
-public class EuropeTax {
 
+
+public class EuropeTax implements TaxCalculator {
+    private static final double VAT_RATE = 0.17;
+    private static final double LUXURY_RATE = 0.25;
+    private static final double LUXURY_FREE_AMOUNT = 100.0;
+
+    @Override
+    public double taxAmount(double taxable) {
+        double vat = taxable * VAT_RATE;
+        double luxury = 0.0;
+
+        if (taxable > LUXURY_FREE_AMOUNT) {
+            luxury = (taxable - LUXURY_FREE_AMOUNT) * LUXURY_RATE;
+        }
+
+        return vat + luxury;
+    }
 }
--- Lab05.3/src/com/javatunes/product/Order.java
+++ Lab05.3/src/com/javatunes/product/Order.java
@@ -8,13 +8,38 @@
  */
 package com.javatunes.product;
 
+import com.javatunes.billing.Location;
+import com.javatunes.billing.TaxCalculator;
+import com.javatunes.billing.TaxCalculatorFactory;
+
 import java.util.Collection;
+import java.util.Locale;
 
 public class Order {
     private String id;
+    private double cost;
+    private Location location;
 
-    public Order(String id) {
+    public Order(String id, Location location) {
         this.id = id;
+        this.location = location;
+    }
+
+    /*
+     * We have 3 options to delegate to a TaxCalculator strategy:
+     * 1. direct instantiation (NOT a good option here)
+     *        if (getLocation() == Location.USA)
+     *
+     * 2. call out to a factory, e.g., TaxCalculatorFactory, and "ask" for it (passing indicator)
+     *
+     * 3. let some other part "inject" it into me and, I'll store it in another field
+     *    private TaxCalculator cal;
+    */
+
+    public double getTax() {
+        TaxCalculator calc = TaxCalculatorFactory.getTaxCalculator(getLocation()); {
+            return calc.taxAmount(getCost());
+        }
     }
 
     /**
@@ -29,10 +54,22 @@ public class Order {
         for (Product product : cartItems) {
             System.out.println(product.getCode());
         }
+        double cartTotal = cart.total();
         System.out.println("Order Total: " + cart.total());
+        this.cost = cart.total();
     }
+
+
 
     public String getId() {
         return id;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public Location getLocation() {
+        return location;
     }
 }
